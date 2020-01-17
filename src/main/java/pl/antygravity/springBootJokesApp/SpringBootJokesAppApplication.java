feb8f16bf9cb7e174ee0b0038232771e8fe18509
rawsk <rawski.andrzej@gmail.com>
2020-01-17 20:55:19 +0100

    Initial commit

---
+++ src/main/java/pl/antygravity/springBootJokesApp/SpringBootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package pl.antygravity.springBootJokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/antygravity/springBootJokesApp/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.antygravity.springBootJokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
